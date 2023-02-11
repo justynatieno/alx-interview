9161bfbb10938bbc79906cb5511e54f7f9745742
jystynatieno <justineomwandho@gmail.com>
2023-02-11 14:11:04 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
 
+def countProcess(num):
+    """ Return list of process until n H """
+    con = 1
+    p_list = []
+    val = num
+    while val != 1:
+        con += 1
+        if val % con == 0:
+            while (val % con == 0 and val != 1):
+                val /= con
+                p_list.append(con)
+
+    return p_list
+
+
 def minOperations(n):
-    if n <= 0:
+    """ Return sum of process until n H """
+    if n < 2 or type(n) is not int:
         return 0
-
-    # Initializing the result with the maximum value
-    result = n
-
-    # Starting from 2 to the square root of n
-    for i in range(2, int(n**0.5) + 1):
-        count = 0
-        temp = n
-
-        # dividing n by i as long as it's divisible
-        while temp % i == 0:
-            count += 1
-            temp = temp // i
-
-        # If n was divided completely by i,
-        if temp == 1:
-            result = min(result, count + minOperations(i))
-
-    # If n is a prime number, return 1
-    if result == n:
-        return 1
-
-    return result
+    results = countProcess(n)
+    return sum(results)
